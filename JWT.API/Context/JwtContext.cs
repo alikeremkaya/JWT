874be07e9be30be910593c52bb06724285baba0c
Ali Kerem Kaya <alikerem.kaya@bilgeadamboost.com>
2024-09-27 11:34:14 +0300

    Add project files.

---
+++ JWT.API/Context/JwtContext.cs
@@ -0,0 +1,19 @@
+﻿using JWT.API.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JWT.API.Context
+{
+    public class JwtContext : IdentityDbContext<AppUser>
+    {
+        public JwtContext(DbContextOptions<JwtContext> options) : base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+           
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ JWT.API/Services/IJwtService.cs
@@ -0,0 +1,10 @@
+﻿using JWT.API.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace JWT.API.Services
+{
+    public interface IJwtService
+    {
+        string GenerateJwtToken(AppUser user);
+    }
+}
